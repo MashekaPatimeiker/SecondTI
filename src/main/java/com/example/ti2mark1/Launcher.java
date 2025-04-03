8ae31b855291bb8069d88b63845b51e5bcb88f39
Даниил <danya.mashevskiy.98@bk.ru>
2025-04-03 17:53:16 +0300

    Better then Good commit

---
+++ src/main/java/com/example/ti2mark1/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.ti2mark1;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args){
+        Application.launch(FrafDisplay.class, args);
+    }
+}
